32d6371b1972e24c645d312c2d1e38536fb61105
Tom Barton <grez.cz@gmail.com>
2016-04-03 00:11:52 +0200

    Fixed autoloading for FW

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,11 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Teddy\Configurator;
-//$configurator->setDebugMode(false);
+$configurator = (new Teddy\Configurator);
 
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__)
+    ->addDirectory(__DIR__ . '/../vendor/teddy/framework/app') // why doesn't autoloader fetch that... check PSR autoloading :/
     ->register();
 
 $container = $configurator->createContainer();
