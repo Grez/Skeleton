efff3f45f0f3fcb6df6e98ee29c5d73d8a213971
Tom Barton <grez.cz@gmail.com>
2016-05-23 02:19:17 +0200

    Added indexes

--- app/entities/User/UserAgent.php
+++ app/entities/User/UserAgent.php
@@ -2,14 +2,15 @@
 
 namespace Game\Entities\User;
 
-use Doctrine\ORM\Mapping as ORM;
 use Kdyby\Doctrine\Entities\Attributes\Identifier;
 use Nette;
+use Teddy\Entities;
+use Doctrine\ORM\Mapping as ORM;
 
 
 
 /**
- * @ORM\Entity
+ * @ORM\Entity()
  */
 class UserAgent extends \Teddy\Entities\User\UserAgent
 {
